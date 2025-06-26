1734d2af3401af6df520b705e55894f498941928
Joe Sortelli <joe@sortelli.com>
2025-06-26 10:07:46 -0700

    Add cursor to setup_vscode_on_mac.sh

--- bin/setup_vscode_on_mac.sh
+++ bin/setup_vscode_on_mac.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env bash
 
-defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
-defaults write com.microsoft.VSCode NSAutomaticPeriodSubstitutionEnabled -bool false
+function set_defaults() {
+  local app_id="$1"
+
+  defaults write "$app_id" ApplePressAndHoldEnabled -bool false
+  defaults write "$app_id" NSAutomaticPeriodSubstitutionEnabled -bool false
+}
+
+set_defaults "com.microsoft.VSCode"            # Vscode
+set_defaults "com.todesktop.230313mzl4w4u92"   # Cursor
+
+# Cursor's app id was found via the following command
+#     % osascript -e 'id of app "Cursor"'
+#     com.todesktop.230313mzl4w4u92
